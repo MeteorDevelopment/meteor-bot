cb36f04e6a6d0a96b87bce2c07d9e8ee455b39bf
MineGame159 <petulko08@gmail.com>
2020-11-03 12:35:37 +0100

    nothing to see here

--- src/main/java/minegame159/meteorbot/Config.java
+++ src/main/java/minegame159/meteorbot/Config.java
@@ -9,6 +9,7 @@ public class Config {
     public static String DISCORD_TOKEN;
     public static String MONGO_URL;
     public static String SERVER_TOKEN;
+    public static String PVP_SERVER_TOKEN;
 
     public static void init() {
         try {
@@ -21,6 +22,7 @@ public class Config {
             DISCORD_TOKEN = properties.getProperty("discord_token");
             MONGO_URL = properties.getProperty("mongo_url");
             SERVER_TOKEN = properties.getProperty("server_token");
+            PVP_SERVER_TOKEN = properties.getProperty("pvp_server_token");
 
             MeteorBot.LOG.info("Loaded config");
         } catch (IOException e) {
--- src/main/java/minegame159/meteorbot/utils/PvpServer.java
+++ src/main/java/minegame159/meteorbot/utils/PvpServer.java
@@ -1,27 +1,26 @@
 package minegame159.meteorbot.utils;
 
 import kong.unirest.Unirest;
+import minegame159.meteorbot.Config;
 
 public class PvpServer {
-    private static final String TOKEN = "M2LoLF4iZSAJt25kF84l";
-
     public static void giveDonator(String uuid) {
         Unirest.post("http://pvp.meteorclient.com:8115/adddonator")
-                .queryString("token", TOKEN)
+                .queryString("token", Config.PVP_SERVER_TOKEN)
                 .queryString("uuid", Utils.newUUID(uuid))
                 .asEmpty();
     }
 
     public static void removeDonator(String uuid) {
         Unirest.post("http://pvp.meteorclient.com:8115/removedonator")
-                .queryString("token", TOKEN)
+                .queryString("token", Config.PVP_SERVER_TOKEN)
                 .queryString("uuid", Utils.newUUID(uuid))
                 .asEmpty();
     }
 
     public static void toggleDonator(String uuid) {
         Unirest.post("http://pvp.meteorclient.com:8115/toggledonator")
-                .queryString("token", TOKEN)
+                .queryString("token", Config.PVP_SERVER_TOKEN)
                 .queryString("uuid", Utils.newUUID(uuid))
                 .asEmpty();
     }
