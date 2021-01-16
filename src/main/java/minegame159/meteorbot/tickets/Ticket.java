b973277d05da46461f54cd7c645ef0c141de7c04
MineGame159 <petulko08@gmail.com>
2021-01-16 14:19:08 +0100

    Fixed pings

--- src/main/java/minegame159/meteorbot/tickets/Ticket.java
+++ src/main/java/minegame159/meteorbot/tickets/Ticket.java
@@ -136,7 +136,8 @@ public class Ticket {
             problem.sendSolution(this);
         }
         else if (stage == Stage.Manual) {
-            channel.sendMessage(embed("You can now talk to the helpers.\n\n" + MeteorBot.HELPER_ROLE.getAsMention()).build()).queue();
+            channel.sendMessage(embed("You can now talk to the helpers.").build()).queue();
+            channel.sendMessage(MeteorBot.HELPER_ROLE.getAsMention()).queue();
         }
     }
 
