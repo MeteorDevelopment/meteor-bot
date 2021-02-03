2335f445ca77894b84a0dc4fd57f1a96c192838d
MineGame159 <petulko08@gmail.com>
2021-02-03 11:00:52 +0100

    REEEEEEEEEEEEEEEEEEEE

--- src/main/java/minegame159/meteorbot/MeteorBot.java
+++ src/main/java/minegame159/meteorbot/MeteorBot.java
@@ -61,9 +61,9 @@ public class MeteorBot extends ListenerAdapter {
         event.getJDA().getPresence().setActivity(Activity.playing("Meteor on Crack!"));
 
         GUILD = JDA.getGuildById(689197705683140636L);
-        MINEGAME = GUILD.retrieveMemberById(205708530408357898L).complete().getUser();
-        SQUID = GUILD.retrieveMemberById(322777907078627328L).complete().getUser();
-        SEASNAIL = GUILD.retrieveMemberById(736954747122352208L).complete().getUser();
+        try { MINEGAME = GUILD.retrieveMemberById(205708530408357898L).complete().getUser(); } catch (Exception ignored) {}
+        try { SQUID = GUILD.retrieveMemberById(322777907078627328L).complete().getUser(); } catch (Exception ignored) {}
+        try { SEASNAIL = GUILD.retrieveMemberById(736954747122352208L).complete().getUser(); } catch (Exception ignored) {}
         MOD_ROLE = GUILD.getRoleById(689197893340758022L);
         HELPER_ROLE = GUILD.getRoleById(799392357157830657L);
 
