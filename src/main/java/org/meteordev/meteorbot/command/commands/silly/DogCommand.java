38ab8bdf42888bbc243f1761c514993b8779913d
Talhoid <68508139+Talhoid@users.noreply.github.com>
2023-10-07 09:39:02 -0400

    fix silly

--- src/main/java/org/meteordev/meteorbot/command/commands/silly/DogCommand.java
+++ src/main/java/org/meteordev/meteorbot/command/commands/silly/DogCommand.java
@@ -17,7 +17,7 @@ public class DogCommand extends Command {
 
     @Override
     public void run(SlashCommandInteractionEvent event) {
-        Unirest.get("https://some-random-api.ml/img/dog").asJsonAsync(response -> {
+        Unirest.get("https://some-random-api.com/img/dog").asJsonAsync(response -> {
             event.reply(response.getBody().getObject().getString("link")).queue();
         });
     }
